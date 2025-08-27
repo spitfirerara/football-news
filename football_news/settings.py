b78d0cf9d947673f202434267d0a06995b0f41b2
spitfirerara <nairaammara25@gmail.com>
2025-08-27 17:08:47 +0700

    Tutorial 0

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "naira-ammara-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
