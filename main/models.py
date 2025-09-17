cf9a0abf08930a57ba9b60ee42f6fe0917cbc8ac
spitfirerara <nairaammara25@gmail.com>
2025-09-17 15:54:29 +0700

    Tutorial 3 selesai

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class News(models.Model):
     CATEGORY_CHOICES = [
@@ -11,6 +12,7 @@ class News(models.Model):
         ('analysis', 'Analysis'),
     ]
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     content = models.TextField()
