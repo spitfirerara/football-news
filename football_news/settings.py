7650723451d20b1be381d412adf3a856566f632a
spitfirerara <nairaammara25@gmail.com>
2025-09-17 16:32:29 +0700

    Menyelesaikan tutorial 3

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "naira-ammara-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://naira-ammara-footballnews.pbp.cs.ui.ac.id/"
+    "https://naira-ammara-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
